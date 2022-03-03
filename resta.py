4f79ee82071e2adb3a4ae44bb2fec72e3236d32f
Johannapluas <99738026+Johannapluas@users.noreply.github.com>
2022-03-03 15:15:45 -0500

    Update resta.py

--- resta.py
+++ resta.py
@@ -1,22 +1,30 @@
-ejercicio1
-#Ejecucion de Resta
 
+#Ejercicio de Resta 1
 t= 24-5
 print(t)
 
-ejercicio2
+
+#Ejercicio de Resta 2
 print('resta:', 100 - 30)
 [output] 70 
 
 
-ejercicio3
+#Ejercicio de Resta 3
 num1 = 50
 num2 = 10
 res1 = num1 - num2
 print(res1)
 
-ejercicio4
+
+#Ejercicio de Resta 4
 c= 64
 result = (c - 12)
 print(result)
 
+
+#Ejercicio de Resta 5
+y=12
+x=1
+
+resta= y-X
+print(resta)
