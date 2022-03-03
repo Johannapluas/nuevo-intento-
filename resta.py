d497d030df79a37369ce3dff28a1f2ec38da09df
Johannapluas <99738026+Johannapluas@users.noreply.github.com>
2022-03-03 14:59:06 -0500

    Create resta.py

---
+++ resta.py
@@ -0,0 +1,22 @@
+ejercicio1
+#Ejecucion de Resta
+
+t= 24-5
+print(t)
+
+ejercicio2
+print('resta:', 100 - 30)
+[output] 70 
+
+
+ejercicio3
+num1 = 50
+num2 = 10
+res1 = num1 - num2
+print(res1)
+
+ejercicio4
+c= 64
+result = (c - 12)
+print(result)
+
